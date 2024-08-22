90879230304fbb25f593eabeb7ae0d4f0a2462e0
scarborough high school <tsnyder@scarboroughschools.org>
2024-08-22 19:54:49 -0400

    additional cors fix

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,3 +1,4 @@
 export WEBUI_AUTH=false
+CORS_ALLOW_ORIGIN=http://localhost:5173;http://localhost:8080
 PORT="${PORT:-8080}"
 uvicorn main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
